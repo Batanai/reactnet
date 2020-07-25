8efe9b6f7dad4fbb02e084a63ddfea784dace5b4
Batanai <31771294+Batanai@users.noreply.github.com>
2020-07-25 11:20:55 +0200

    End of section 11

--- client/src/features/home/HomePage.tsx
+++ client/src/features/home/HomePage.tsx
@@ -13,7 +13,7 @@ const HomePage = () => {
             alt='logo'
             style={{ marginBottom: 12 }}
           />
-          Reactnet
+          Reactivities
         </Header>
         <Header as='h2' inverted content='Welcome to Reactivities' />
         <Button as={Link} to='/activities' size='huge' inverted>
