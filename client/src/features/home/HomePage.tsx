8c736274d4223ac1940dd0f0398c7da306b435f1
Batanai <31771294+Batanai@users.noreply.github.com>
2020-07-28 09:48:09 +0200

    End of section 20

--- client/src/features/home/HomePage.tsx
+++ client/src/features/home/HomePage.tsx
@@ -1,32 +1,47 @@
-import React, { useContext, useEffect } from 'react';
-import { Grid } from 'semantic-ui-react';
-import ActivityList from './ActivityList';
-import { observer } from 'mobx-react-lite';
-import LoadingComponent from '../../../app/layout/LoadingComponent';
-import { RootStoreContext } from '../../../app/stores/rootStore';
+import React, { useContext, Fragment } from 'react';
+import { Container, Segment, Header, Button, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { RootStoreContext } from '../../app/stores/rootStore';
+import LoginForm from '../user/LoginForm';
+import RegisterForm from '../user/RegisterForm';
 
-const ActivityDashboard: React.FC = () => {
-
+const HomePage = () => {
   const rootStore = useContext(RootStoreContext);
-  const {loadActivities, loadingInitial} = rootStore.activityStore;
-
-  useEffect(() => {
-    loadActivities();
-  }, [loadActivities]);
-
-  if (loadingInitial)
-    return <LoadingComponent content='Loading activities' />;
-
+  const { user, isLoggedIn } = rootStore.userStore;
+  const {openModal} = rootStore.modalStore;
   return (
-    <Grid>
-      <Grid.Column width={10}>
-        <ActivityList />
-      </Grid.Column>
-      <Grid.Column width={6}>
-        <h2>Activity filters</h2>
-      </Grid.Column>
-    </Grid>
+    <Segment inverted textAlign='center' vertical className='masthead'>
+      <Container text>
+        <Header as='h1' inverted>
+          <Image
+            size='massive'
+            src='/assets/logo.png'
+            alt='logo'
+            style={{ marginBottom: 12 }}
+          />
+          Reactivities
+        </Header>
+        {isLoggedIn && user ? (
+          <Fragment>
+            <Header as='h2' inverted content={`Welcome back ${user.displayName}`} />
+            <Button as={Link} to='/activities' size='huge' inverted>
+              Go to activities!
+            </Button>
+          </Fragment>
+        ) : (
+          <Fragment>
+          <Header as='h2' inverted content={`Welcome to Reactivitities`} />
+          <Button onClick={() => openModal(<LoginForm />)} size='huge' inverted>
+            Login
+          </Button>
+          <Button onClick={() => openModal(<RegisterForm />)} size='huge' inverted>
+            Register
+          </Button>
+        </Fragment>
+        )}
+      </Container>
+    </Segment>
   );
 };
 
-export default observer(ActivityDashboard);
+export default HomePage;
